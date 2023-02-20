3ff88ad2decaa15453a0618d0fffd24b7311ad96
Kevin Salazar <deeiqh@gmail.com>
2023-02-19 19:16:23 -0500

    chore: add test to rest otp-guards package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { redisStore } from 'cache-manager-redis-store';
+import { CommonModule } from 'otp-guards';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [
+    CommonModule.forRoot({
+      otpMinutesToExpire: 0.523,
+      cacheConfig: { store: redisStore },
+      kafkaConfig: {
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'client',
+            brokers: ['inspiron:9092'],
+          },
+          consumer: {
+            groupId: 'test-group',
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
